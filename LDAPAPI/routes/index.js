6a7ee6be63fd9e637783271c745973606e45cd7b
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-07-23 18:10:42 +0200

    auth set up

--- LDAPAPI/routes/index.js
+++ LDAPAPI/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import users from './users.js';
 
+import { authRouter, verifyTokenMiddleware } from './auth.js';
+
 const router = Router();
 
-router.use('/users', users);
+router.use('/users', verifyTokenMiddleware('admin'), users); //when adding the middleware it needs the required role so if we have levels of access
+router.use('/login', authRouter);
 
 export default router
